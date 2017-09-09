0b3e7078a512c79faba0106506c9535e0acae741
Josueemx <Morales@MacBook-de-Josue-1659.local>
2017-09-09 10:34:35 -0600

    jar
    ahora si

--- src/regexvalidator/RegexValidator.java
+++ src/regexvalidator/RegexValidator.java
@@ -32,7 +32,7 @@ public class RegexValidator {
                 System.out.println("Se ha detectado: "+(String)results.get(1));            
             else
                 if(!input.equals("salir"))
-                    System.out.println("No se introdujo una cadena invalida.");            
+                    System.out.println("No se introdujo una cadena valida.");            
         }
         while(!input.equals("salir"));
         System.out.println("bye");
